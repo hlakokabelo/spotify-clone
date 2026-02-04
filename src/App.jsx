10f3a21d20cd7f9e0887dba403ff0cc900edd034
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-02-04 15:46:27 +0200

    Display component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import SideBar from './components/SideBar'
 import Player from './components/Player'
+import Display from './components/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <SideBar />
+        <Display />
       </div>
-      <Player/>
+      <Player />
     </div>
   )
 }
