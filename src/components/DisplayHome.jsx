098e999a520712ca3059f8a7edba724b9fcfc1d3
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-02-04 18:34:22 +0200

    added Album & song components

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData, songsData } from '../assets/assets'
+import Albumitem from './Albumitem'
+import SongItems from './SongItems'
 
 const DisplayHome = () => {
-  return (
-    <>
-      <Navbar/>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Featured charts</h1>
+                <div className="flex overflow-auto">
+                    {albumsData.map((item, index) => (
+                        <Albumitem key={index} image={item.image} name={item.name} desc={item.desc} id={item.id} />
+                    ))}
+                </div>
+
+            </div>
+
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Todays biggest hits</h1>
+                <div className="flex overflow-auto">
+                    {songsData.map((item, index) => (
+                        <SongItems key={index} image={item.image} name={item.name} desc={item.desc} id={item.id} />
+                    ))}
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default DisplayHome
